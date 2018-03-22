90c7767ec1508615bb1342640756cbe6c18a097f
Aubrey Taylor <aubricus@gmail.com>
2018-03-21 22:13:10 -0700

    feature(clickfile): Add Clickfile loading

--- klak/cli.py
+++ klak/cli.py
@@ -2,16 +2,40 @@
 
 """Console script for klak."""
 import sys
+import os
+from importlib.util import spec_from_loader, module_from_spec
+from importlib.machinery import SourceFileLoader
 import click
 
 
-@click.command()
-def main(args=None):
-    """Console script for klak."""
-    click.echo("Replace this message by putting your code into "
-               "klak.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+def import_module_from_file(module_name, path):
+    """Load a modules from a raw file path."""
+    loader = SourceFileLoader(module_name, path)
+    spec = spec_from_loader(module_name, loader)
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    return module
+
+
+def import_clickfile():
+    """Import a python file named Clickfile from os.getcwd()."""
+    cwd = os.getcwd()
+    clickfile_path = os.path.realpath(os.path.join(cwd, "Clickfile"))
+    if os.path.isfile(clickfile_path):
+        return import_module_from_file("usr_cmd", clickfile_path)
+
+
+@click.group()
+def root():
+    """Click n' Klak"""
     return 0
+
+
+def main():
+    """Console script for klak."""
+    import_clickfile()
+    return root()
 
 
 if __name__ == "__main__":
--- tests/test_klak.py
+++ tests/test_klak.py
@@ -1,38 +1,62 @@
 #!/usr/bin/env python
+# pylint: disable=no-member
 # -*- coding: utf-8 -*-
 
 """Tests for `klak` package."""
 
+import os
+import enum
+import shutil
+import tempfile
 import pytest
-
 from click.testing import CliRunner
-
+from path import Path
 from klak import klak
 from klak import cli
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def invoke(*args):
+    """Provide convenience call to test klak cli."""
+    runner = CliRunner()
+    return runner.invoke(cli.root, args)
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+class Paths(enum.Enum):
+    """Test Paths."""
+
+    FilePath = Path(__file__)
+    FileDir = FilePath.dirname()
+    MocksDir = Path(os.path.join(FileDir, "mocks"))
+    MockClickfilePath = Path(os.path.join(MocksDir, "Clickfile"))
+
+    @staticmethod
+    def get_clickfile_path() -> Path:
+        """Return MockClickfilePath.value."""
+        return Paths.MockClickfilePath.value
 
 
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(cli.root)
     assert result.exit_code == 0
-    assert 'klak.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+
+
+def test_clickfile():
+    """Test Clickfile."""
+    runner = CliRunner()
+    with runner.isolated_filesystem() as temp_dir:
+        _tmp = Path(temp_dir)
+        _clickfile = Paths.get_clickfile_path().copy(_tmp)
+
+        assert _clickfile.isfile()
+
+        # Call import_clickfile manually
+        # since the CliRunner cannot call cli.main
+        cli.import_clickfile()
+
+        # Our custom command hello_world should now be available
+        result = invoke("hello_world")
+
+        assert result.exit_code == 0
+        assert "hi!" in result.output
