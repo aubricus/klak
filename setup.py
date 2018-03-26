2f2876e2c3e6f1105933e933193ad232639b08a8
Aubrey Taylor <aubricus@gmail.com>
2018-03-26 09:49:49 -0700

    hotfix/add-pathpy (#6)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ['Click>=6.0', 'path.py>=11.0']
 
 setup_requirements = ['pytest-runner', ]
 
