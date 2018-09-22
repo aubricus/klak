64dac125ab9ed1c8dbda3f389aaf24f2f1ffc089
Aubrey Taylor <aubricus@gmail.com>
2018-09-22 15:07:17 -0700

    chore(klak): Fix pylint errors, setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
-    readme = readme_file.read()
+    README = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+    HISTORY = history_file.read()
 
-requirements = ['Click>=6.0', 'path.py>=11.0']
+REQUIREMENTS = ['Click>=6.0', 'path.py>=11.0']
 
-setup_requirements = ['pytest-runner', ]
+SETUP_REQUIREMENTS = ['pytest-runner', ]
 
-test_requirements = ['pytest', ]
+TEST_REQUIREMENTS = ['pytest', ]
 
 setup(
     author="Aubrey Taylor",
@@ -34,16 +34,16 @@ setup(
             'klak=klak.cli:main',
         ],
     },
-    install_requires=requirements,
+    install_requires=REQUIREMENTS,
     license="MIT License",
-    long_description=readme + '\n\n' + history,
+    long_description=README + '\n\n' + HISTORY,
     include_package_data=True,
     keywords='klak',
     name='klak',
     packages=find_packages(include=['klak']),
-    setup_requires=setup_requirements,
+    setup_requires=SETUP_REQUIREMENTS,
     test_suite='tests',
-    tests_require=test_requirements,
+    tests_require=TEST_REQUIREMENTS,
     url='https://github.com/aubricus/klak',
     version='0.2.3',
     zip_safe=False,
