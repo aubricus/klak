08aed134995d289472ad9eec9caddff91eb19488
Aubrey Taylor <aubricus@gmail.com>
2018-09-22 15:13:55 -0700

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=TEST_REQUIREMENTS,
     url='https://github.com/aubricus/klak',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
