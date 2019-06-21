01e0627ba273d3cdf54a05411e0b14d99ce5f6e2
Aubrey Taylor <aubricus@gmail.com>
2019-06-20 22:53:45 -0700

    Add -v/--version to root cli
    This commit also refactors klak.root to klak.cli
    for a slightly nicer experience.

--- klak/cli.py
+++ klak/cli.py
@@ -1,40 +1,22 @@
-# -*- coding: utf-8 -*-
-
 """Console script for klak."""
+
 import sys
-import os
-from importlib.util import spec_from_loader, module_from_spec
-from importlib.machinery import SourceFileLoader
+import logging
 import click
-from path import Path
+from .klak import import_clickfile, get_version
 
 
-def import_module_from_file(module_name, path):
-    """Load a modules from a raw file path."""
-    loader = SourceFileLoader(module_name, path)
-    spec = spec_from_loader(module_name, loader)
-    module = module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    return module
+log = logging.getLogger(__name__)
 
 
-def import_clickfile():
-    """Import a python file named Clickfile from os.getcwd()."""
-    cwd = Path(os.getcwd())
-    clickfile_path = Path(os.path.join(cwd, "Clickfile"))
-
-    if clickfile_path.isfile():
-        return import_module_from_file("clickfile_cmds", str(clickfile_path))
-    else:
-        raise click.exceptions.FileError(
-            clickfile_path, hint="Error: Could not find Clickfile!"
-        )
-
-
-@click.group()
-def root():
+@click.group(invoke_without_command=True)
+@click.option("-v", "--version", is_flag=True, help="Print current version.")
+@click.pass_context
+def cli(ctx, version):
     """Click n' Klak."""
+    if ctx.invoked_subcommand is None and version:
+        click.secho(get_version(), fg="green")
+
     return 0
 
 
@@ -45,7 +27,7 @@ def main():
     except click.exceptions.FileError as exception:
         click.secho("\n{error}\n".format(error=str(exception)), fg="red", bold=True)
         return 1
-    return root()
+    return cli()
 
 
 if __name__ == "__main__":
--- klak/klak.py
+++ klak/klak.py
@@ -1,3 +1,48 @@
-# -*- coding: utf-8 -*-
-
 """Main module."""
+
+import os
+import logging
+import toml
+from importlib.util import spec_from_loader, module_from_spec
+from importlib.machinery import SourceFileLoader
+from typing import MutableMapping
+from pathlib import Path
+
+
+log = logging.getLogger(__name__)
+
+
+def get_version() -> str:
+    """Return current klak version."""
+    pyproject_path = Path(__file__).parent.parent.joinpath("pyproject.toml")
+    if pyproject_path.exists():
+        pyproject = toml.load(str(pyproject_path))
+        version = pyproject["tool"]["poetry"]["version"]
+    else:
+        log.error("Could not read pyproject.toml to determine version.")
+        version = "Unknown"
+
+    return version
+
+
+def import_clickfile():
+    """Import a python file named Clickfile from os.getcwd()."""
+    cwd = Path.cwd()
+    clickfile_path = cwd.joinpath("Clickfile")
+
+    if clickfile_path.is_file():
+        return import_module_from_file("Clickfile", str(clickfile_path))
+    else:
+        raise click.exceptions.FileError(
+            clickfile_path, hint="Error: Could not find Clickfile!"
+        )
+
+
+def import_module_from_file(module_name, path):
+    """Load a modules from a raw file path."""
+    loader = SourceFileLoader(module_name, path)
+    spec = spec_from_loader(module_name, loader)
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    return module
