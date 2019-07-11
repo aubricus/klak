a0e751730fdb36c9b5b0f9405aa21aae48aadfe9
Aubrey Taylor <558321+aubricus@users.noreply.github.com>
2019-07-10 23:03:02 -0700

    Feature: Enable Import Modules (#229)
    * Adjust formatting, klak.py
    
    * Add append_path util
    
    * Update cli.py to use append_path
    
    * Update README.md
    
    * Update tests

--- klak/klak.py
+++ klak/klak.py
@@ -1,12 +1,14 @@
 """Main module."""
 
 import os
+import sys
 import logging
 import click
 from importlib.util import spec_from_loader, module_from_spec
 from importlib.machinery import SourceFileLoader
 from typing import MutableMapping
 from pathlib import Path
+from contextlib import contextmanager
 
 
 log = logging.getLogger(__name__)
@@ -15,11 +17,13 @@ VERSION = "0.4.3"
 
 def get_version() -> str:
     """Return current klak version."""
+
     return VERSION
 
 
 def import_clickfile():
     """Import a python file named Clickfile from os.getcwd()."""
+
     cwd = Path.cwd()
     clickfile_path = cwd.joinpath("Clickfile")
 
@@ -33,9 +37,20 @@ def import_clickfile():
 
 def import_module_from_file(module_name, path):
     """Load a modules from a raw file path."""
+
     loader = SourceFileLoader(module_name, path)
     spec = spec_from_loader(module_name, loader)
     module = module_from_spec(spec)
     spec.loader.exec_module(module)
 
     return module
+
+
+@contextmanager
+def append_path(path: Path):
+    """Context wrapper to temporarily append a directory to sys.path."""
+
+    _path = str(path.resolve())
+    sys.path.append(_path)
+    yield
+    sys.path.remove(_path)
--- tests/test_klak.py
+++ tests/test_klak.py
@@ -6,6 +6,7 @@ import shutil
 import tempfile
 import pytest
 import click
+import typing
 from pathlib import Path
 from click.testing import CliRunner
 from klak import klak, cli
@@ -23,42 +24,86 @@ class Paths(enum.Enum):
     FilePath = Path(__file__)
     FileDir = FilePath.parent
     MocksDir = FileDir.joinpath("mocks")
-    MockClickfilePath = MocksDir.joinpath("Clickfile")
+    ClickfileSimple = MocksDir.joinpath("Clickfile.simple")
+    ClickfileModules = MocksDir.joinpath("Clickfile.modules")
+    Tasks = MocksDir.joinpath("tasks")
 
-    @staticmethod
-    def get_clickfile_path() -> Path:
-        """Return MockClickfilePath.value."""
-        return Paths.MockClickfilePath.value
+
+def copy(src: Path, dest: Path) -> Path:
+    assert src.exists()
+
+    if src.is_file():
+        shutil.copy(str(src), str(dest))
+    if src.is_dir():
+        shutil.copytree(str(src), str(dest))
+
+    assert dest.exists()
+    return dest
+
+
+def import_clickfile(tmp_dir: Path) -> None:
+    """Import Clickfile at tmp_dir."""
+
+    with klak.append_path(tmp_dir):
+        klak.import_clickfile()
 
 
 def test_command_line_interface():
     """Test the CLI."""
+
     runner = CliRunner()
     result = runner.invoke(cli.cli)
     assert result.exit_code == 0
 
 
-def test_import_clickfile():
+def setup_test_clickfile(tmp_dir: Path) -> None:
+    """Setup def test_clickfile."""
+
+    clickfile = Paths.ClickfileSimple.value
+    copy(clickfile, tmp_dir.joinpath("Clickfile"))
+
+
+def test_clickfile():
     """Test Clickfile."""
+
     runner = CliRunner()
-    with runner.isolated_filesystem() as temp_dir:
-        _tmp = Path(temp_dir)
 
-        shutil.copy(str(Paths.get_clickfile_path()), str(_tmp))
+    with runner.isolated_filesystem() as d:
+        tmp_dir = Path(d)
+        setup_test_clickfile(tmp_dir)
 
-        _clickfile = _tmp.joinpath("Clickfile")
-        assert _clickfile.is_file()
+        import_clickfile(tmp_dir)
 
-        # Call import_clickfile manually
-        # since the CliRunner cannot use the runner to invoke
-        # cli.main directly.
-        klak.import_clickfile()
-
-        # Our custom command hello_world should now be available
         result = invoke("hello-world")
 
         assert result.exit_code == 0
         assert "hi!" in result.output
+
+
+def setup_test_modules(tmp_dir: Path) -> None:
+    """Setup def test_modules."""
+
+    clickfile = Paths.ClickfileModules.value
+    tasks = Paths.Tasks.value
+
+    copy(clickfile, tmp_dir.joinpath("Clickfile"))
+    copy(tasks, tmp_dir.joinpath("tasks"))
+
+
+def test_modules():
+    """Test Clickfile with module."""
+
+    runner = CliRunner()
+
+    with runner.isolated_filesystem() as d:
+        tmp_dir = Path(d)
+        setup_test_modules(tmp_dir)
+
+        import_clickfile(tmp_dir)
+
+        result = invoke("ducks", "quack")
+        assert result.exit_code == 0
+        assert "Quack quack!" in result.output
 
 
 def test_import_missing_clickfile():
